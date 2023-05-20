bfdbb356f8702464e0eb4416ab64599f8abfe0d7
Edwin Evia <edwinevi@student.21-school.ru>
2023-05-20 14:44:34 +0300

    create void for check valid func

--- src/model/calculator.cc
+++ src/model/calculator.cc
@@ -1,43 +1,43 @@
 #include "calculator.h"
 
 
-int getPostfix(const char *infix, char *postfix) {
-  int good_to_go = 1;
-  int index = 0;
-  int infix_len = (int)strlen(infix);
-  char in = {0};
-  char add = {0};
-  operation *head = NULL;
-  char prev = {0};
+// int getPostfix(const char *infix, char *postfix) {
+//   int good_to_go = 1;
+//   int index = 0;
+//   int infix_len = (int)strlen(infix);
+//   char in = {0};
+//   char add = {0};
+//   operation *head = NULL;
+//   char prev = {0};
 
-  for (int i = 0; i < infix_len && good_to_go; i++) {
-    in = infix[i];
-    if (in == 'x') {
-      postfix[index++] = in;
-      postfix[index++] = ' ';
-    } else if (in == '(') {
-      good_to_go = pushOp(&head, in);
-    } else if (isFunc(in)) {
-      good_to_go = validateFunc(infix, &i, &add);
-      if (good_to_go) good_to_go = pushOp(&head, add);
-    } else if (isOperator(in)) {
-      good_to_go = validateOperator(infix, &i, head, &add, prev);
-      if (good_to_go) good_to_go = processOperator(&head, postfix, &index, add);
-    } else if (in == ')') {
-      good_to_go = processBracket(&head, postfix, &index);
-    } else if (isdigit(in)) {
-      good_to_go = addDigit(infix, &i, postfix, &index);
-    } else if (in != ' ') {
-      good_to_go = 0;
-    }
+//   for (int i = 0; i < infix_len && good_to_go; i++) {
+//     in = infix[i];
+//     if (in == 'x') {
+//       postfix[index++] = in;
+//       postfix[index++] = ' ';
+//     } else if (in == '(') {
+//       good_to_go = pushOp(&head, in);
+//     } else if (isFunc(in)) {
+//       good_to_go = validateFunc(infix, &i, &add);
+//       if (good_to_go) good_to_go = pushOp(&head, add);
+//     } else if (isOperator(in)) {
+//       good_to_go = validateOperator(infix, &i, head, &add, prev);
+//       if (good_to_go) good_to_go = processOperator(&head, postfix, &index, add);
+//     } else if (in == ')') {
+//       good_to_go = processBracket(&head, postfix, &index);
+//     } else if (isdigit(in)) {
+//       good_to_go = addDigit(infix, &i, postfix, &index);
+//     } else if (in != ' ') {
+//       good_to_go = 0;
+//     }
 
-    prev = in;
-  }
-  if (good_to_go) good_to_go = processRemains(&head, postfix, &index);
-  if (good_to_go) good_to_go = validateRPN(postfix);
+//     prev = in;
+//   }
+//   if (good_to_go) good_to_go = processRemains(&head, postfix, &index);
+//   if (good_to_go) good_to_go = validateRPN(postfix);
 
-  return good_to_go;
-}
+//   return good_to_go;
+// }
 
 namespace s21 {
 void Calculator::Calculate() {
@@ -48,22 +48,61 @@ void Calculator::Calculate() {
 void Calculator::GetPostfix() {
     std::cout << infix_ << std::endl;
     // infix_ +
-    cur_pos_ = infix.begin();
-    for (auto it = infix_.begin(); it != infix_.end(); it++) {
-        if (*it == 'x') {
-            postfix_.push_back(*it);
+    // cur_pos_ = infix_.begin();
+    for (cur_pos_ = infix_.begin(); cur_pos_ != infix_.end() && good_to_go_; cur_pos_++) {
+        if (*cur_pos_ == 'x') {
+            postfix_.push_back(*cur_pos_);
             postfix_.push_back(' ');
-        } else if (*it == ')') {
+        } else if (*cur_pos_ == ')') { // возможно тоже заменит на enum
             operations_.push(BRACKET);
-        } else if (IsFunc(*it)) {
-
+        } else if (IsValidFunc(*cur_pos_)) {
+            good_to_go_ = IsValidFunc();
+            if (good_to_go_) good_to_go_ = 
         }
     }
 }
 
-bool Calculator::IsFunc(const char check) {
+
+
+bool Calculator::IsValidFunc(const char check) {
+    // перепиши и используй итератор
     std::string funcs = "cstal";
     return (funcs.find(check) != std::string::npos);
+}
+
+bool Calculator::IsValidFunc() {
+    if (*cur_pos_ == 'c' && *(cur_pos_ + 1) == 'o' && *(cur_pos_ + 2) == 's') {
+        op_ = COS;
+        cur_pos_ += 2;
+    } else if (*cur_pos_ == 's' && *(cur_pos_ + 1) == 'i' && *(cur_pos_ + 2) == 'n') {
+        op_ = SIN;
+        cur_pos_ += 2;
+    } else if (*cur_pos_ == 't' && *(cur_pos_ + 1) == 'a' && *(cur_pos_ + 2) == 'n') {
+        op_ = TAN;
+        cur_pos_ += 2;
+    } else if (*cur_pos_ == 'a' && *(cur_pos_ + 1) == 'c' && *(cur_pos_ + 2) == 'o' && *(cur_pos_ + 3) == 's') {
+        op_ = ACOS;
+        cur_pos_ += 3;
+    } else if (*cur_pos_ == 'a' && *(cur_pos_ + 1) == 's' && *(cur_pos_ + 2) == 'i' && *(cur_pos_ + 3) == 'n') {
+        op_ = ASIN;
+        cur_pos_ += 3;
+    } else if (*cur_pos_ == 'a' && *(cur_pos_ + 1) == 't' && *(cur_pos_ + 2) == 'a' && *(cur_pos_ + 3) == 'n') {
+        op_ = ATAN;
+        cur_pos_ += 3;
+    } else if (*cur_pos_ == 's' && *(cur_pos_ + 1) == 'q' && *(cur_pos_ + 2) == 'r' && *(cur_pos_ + 3) == 't') {
+        op_ = ATAN;
+        cur_pos_ += 3;
+    } else if (*cur_pos_ == 'l' && *(cur_pos_ + 1) == 'n') {
+        op_ = LN;
+        cur_pos_ += 1;
+    } else if (*cur_pos_ == 'l' && *(cur_pos_ + 1) == 'o' && *(cur_pos_ + 2) == 'g') {
+        op_ = LOG;
+        cur_pos_ += 2;
+    } else {
+        return false;
+    }
+
+    return true;
 }
 
 };
--- src/model/calculator.h
+++ src/model/calculator.h
@@ -5,10 +5,19 @@ namespace s21 {
 
     // add enums
 class Calculator {
-    enum Operation {
+    enum Operation  {
         BRACKET,
-
+        COS,
+        SIN,
+        TAN,
+        ACOS,
+        ASIN,
+        ATAN,
+        SQRT,
+        LN,
+        LOG
     };
+
     public:
         Calculator() = delete;
         Calculator(const std::string infix) : infix_ (infix) {};
@@ -16,15 +25,19 @@ class Calculator {
         Calculator(Calculator &&other) = delete;
 
         void Calculate();
-        bool IsFunc(const char check);
-        bool IsValidFunc()
+        // стоит объединить функции ниже
+        bool IsValidFunc(const char check);
+        bool IsValidFunc();
     private:
         void GetPostfix();
-        std::iterator cur_pos_;
+        // переделать на текущий итератор
+        std::string::iterator cur_pos_;
         std::string postfix_;
         std::string infix_;
         std::stack<Operation> operations_;
         std::stack<double> calculate_;
+        bool good_to_go_ = true;
+        Operation op_;
 };
 
 };
