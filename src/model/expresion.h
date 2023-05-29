873d4bf841081cc1769c527d1085e26fedc741c1
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2023-05-29 15:40:52 +0300

    rename class Calculator to Expresion

---
+++ src/model/expresion.h
@@ -0,0 +1,74 @@
+// add headers
+#include <iostream>
+#include <stack>
+namespace s21 {
+
+// add enums
+class Expresion {
+  enum Operation {
+    BRACKET,
+    COS,
+    SIN,
+    TAN,
+    ACOS,
+    ASIN,
+    ATAN,
+    SQRT,
+    LN,
+    LOG,
+    // between lexems
+    PLUS,
+    MINUS,
+    MOD,
+    MUL,
+    DIV,
+    EXP,
+    // unar
+    UNARMINUS,
+
+  };
+
+ public:
+  Expresion() = delete;
+  Expresion(const std::string infix) : infix_(infix){};
+  Expresion(const Expresion &other) = delete;
+  Expresion(Expresion &&other) = delete;
+
+  void Calculate();
+  // стоит объединить функции ниже
+  bool IsValidFunc(const char check);
+  bool IsValidFunc();
+
+ private:
+  class Lexema {
+   public:
+    Lexema(Operation operation) : operation_(operation) {}
+    int GetPriority() { return priority_; }
+
+   private:
+    Operation operation_{};
+    unsigned int priority_{};
+    void SetPrirority() {
+      if (operation_ == PLUS || operation_ == MINUS || operation_ == MOD) {
+        priority_ = 1;
+      } else if (operation_ == MUL || operation_ == DIV) {
+        priority_ = 2;
+      } else if (operation_ == EXP || operation_ == UNARMINUS) {
+        priority_ = 3;
+      } else {
+        priority_ = 4;
+      }
+    }
+  };
+  void GetPostfix();
+  // переделать на текущий итератор
+  std::string::iterator cur_pos_;
+  std::string postfix_;
+  std::string infix_;
+  std::stack<Operation> operations_;
+  std::stack<double> calculate_;
+  bool good_to_go_ = true;
+  Operation op_;
+};
+
+};  // namespace s21
